9f302a735d4fbf524ac35c049cdcaf460921bfec
Mitchel Foster <mitchfoster3@gmail.com>
2017-08-03 13:34:27 -0600

    Finished work for today on the class example

--- app/components/starship/starshipComp.js
+++ app/components/starship/starshipComp.js
@@ -1,7 +1,9 @@
 angular.module('starwars').component('starship', {
 
     controller:function(){
-
+        this.goToHyperSpace = ()=>{
+            alert(`The ${this.details.name} is going to hyperspace!`)
+        }
     },
     bindings:{
         owner:'@',
