0e4a4c72b6117447b4045657b35bc04609831679
Mitchel Foster <mitchfoster3@gmail.com>
2017-08-03 11:12:44 -0600

    We have changed the starshipTemplate to be a component

--- app/components/starship/starshipComp.js
+++ app/components/starship/starshipComp.js
@@ -1,1 +1,19 @@
-angular.module('starwars').component('starship')
+angular.module('starwars').component('starship', {
+
+    controller:function(){
+
+    },
+    bindings:{
+        owner:'@',
+        details:'<',
+        onPurchase:'&',
+    },
+    controllerAs:'ship',
+    templateUrl:'app/components/starship/starshipTemplate.html',
+
+
+})
+
+//BINDING @ is string binding
+//BINDING < is one way binding; pass through a variable which could be an object, array, etc.
+//BINDING @ is action binding; pass through javascript or a function
